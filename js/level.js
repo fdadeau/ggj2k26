417473f78a99cade9c88409cb584cd2381a7c023
NwaitDev <nathangallone@hotmail.com>
2026-02-01 00:20:11 +0100

    Normalization of camera speed done, new bug found (not dying from the bottom camera border), see lvl 2

--- js/level.js
+++ js/level.js
@@ -43,10 +43,16 @@ export class Level {
 
     update(dt, keys) {
         if (this.cameraPath.length == 0) return;
-        const target = { x: this.cameraPath[0].x * SIZE, y: this.cameraPath[0].y, speed: this.cameraPath[0].speed };
-        const cameraDirection = { x: target.x-this.camera.x, y: target.y-this.camera.y };
-        const normalCameraDirection = {x:cameraDirection.x/Math.sqrt(cameraDirection.x*cameraDirection.x + cameraDirection.y*cameraDirection.y)};
-
+        const target = { x: this.cameraPath[0].x, y: this.cameraPath[0].y, speed: this.cameraPath[0].speed };
+        const cameraDirection = { 
+            x: target.x-this.camera.x, 
+            y: target.y-this.camera.y 
+        };
+        const normalCameraDirection = {
+            x: cameraDirection.x/Math.sqrt(cameraDirection.x*cameraDirection.x + cameraDirection.y*cameraDirection.y),
+            y: cameraDirection.y/Math.sqrt(cameraDirection.x*cameraDirection.x + cameraDirection.y*cameraDirection.y)
+        };
+        //alert(JSON.stringify(target)+JSON.stringify(cameraDirection)+JSON.stringify(normalCameraDirection));
         this.camera.x += normalCameraDirection.x * CAMERA_SPEED * target.speed * dt;
         this.camera.y += normalCameraDirection.y * CAMERA_SPEED * target.speed * dt;
         if (Math.abs(this.camera.x - target.x) < EPSILON && Math.abs(this.camera.y - target.y) < EPSILON) {
