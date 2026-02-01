3fa6fb13095ea40043becfc3b7ea3efc3bbb7482
thakkar <tayeb.hakkar@edu.univ-fcomte.fr>
2026-02-01 13:49:27 +0100

    Add favicon & resize masks info

--- js/player.js
+++ js/player.js
@@ -390,8 +390,8 @@ export class Player {
             PLAYER_H
         );
 
-        ctx.drawImage(data[`frame-${this.mask}`], 10, 10, 30, 30);
-        ctx.drawImage(data[`frame-${this.mask2}`], 50, 20, 20, 20);
+        ctx.drawImage(data[`frame-${this.mask}`], 20, 10, 40, 40);
+        ctx.drawImage(data[`frame-${this.mask2}`], 70, 20, 30, 30);
 
         //ctx.fillText(JSON.stringify(this.dash), 10, 50)
 
