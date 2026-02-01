650e223b92a370c239e2fa4a4396ff0a32ef0d9a
Robin GRAPPE <robin.grappe@edu.univ-fcomte.fr>
2026-02-01 14:35:43 +0100

    Bird level section

--- js/LEVELS.js
+++ js/LEVELS.js
@@ -1,11 +1,11 @@
 export const LEVELS = {
     1: {
         "player":{
-            "startPosition":{"x":23,"y":5},
+            "startPosition":{"x":73,"y":3},
             // "startPosition":{"x":8,"y":3},
         },
         "camera":{
-            "startPosition":{"x":23 ,"y":5},
+            "startPosition":{"x":75 ,"y":5},
             // "startPosition":{"x":8 ,"y":5},
             "path":[
                 {
@@ -31,7 +31,7 @@ export const LEVELS = {
                 "y":2,
                 "startLiving":0,
                 "stopLiving":undefined,
-                "width": 2,
+                "width":2,
                 "height":1
             },
             {
@@ -40,7 +40,7 @@ export const LEVELS = {
                 "y":2,
                 "startLiving":0,
                 "stopLiving":undefined,
-                "width": 2,
+                "width":2,
                 "height":2
             },
             {
@@ -49,25 +49,25 @@ export const LEVELS = {
                 "y":2,
                 "startLiving":0,
                 "stopLiving":undefined,
-                "width": 5,
+                "width":5,
                 "height":3
             },
-            {
-                "kind":"Breakable",
-                "x":22,
-                "y":5,
-                "startLiving":0,
-                "stopLiving": "punched",
-                "width": 1,
-                "height": 2
-            },
+            // {
+            //     "kind":"Breakable",
+            //     "x":22,
+            //     "y":5,
+            //     "startLiving":0,
+            //     "stopLiving": "punched",
+            //     "width": 1,
+            //     "height": 2
+            // },
             {
                 "kind":"Permanent",
                 "x":28,
                 "y":3,
                 "startLiving":0,
                 "stopLiving":undefined,
-                "width": 3,
+                "width":3,
                 "height":1
             },
             {
@@ -76,7 +76,7 @@ export const LEVELS = {
                 "y":0,
                 "startLiving":0,
                 "stopLiving":undefined,
-                "width": 5,
+                "width":16,
                 "height":3
             },
             {
@@ -88,26 +88,107 @@ export const LEVELS = {
                 "startLiving":0
             },
             {
-                "kind":"Mask",
-                "type":"ninja",
-                "blur": "rgba(0, 0, 0, 0.9)",
-                "x":36,
+                "kind":"Permanent",
+                "x":45,
+                "y":3,
+                "startLiving":0,
+                "stopLiving":undefined,
+                "width":5,
+                "height":3
+            },
+            {
+                "kind":"Permanent",
+                "x":52,
                 "y":5,
-                "startLiving":0
+                "startLiving":0,
+                "stopLiving":undefined,
+                "width":1,
+                "height":10
+            },
+            {
+                "kind":"Permanent",
+                "x":54,
+                "y":0,
+                "startLiving":0,
+                "stopLiving":undefined,
+                "width":5,
+                "height":2
+            },
+            {
+                "kind":"Permanent",
+                "x":62,
+                "y":0,
+                "startLiving":0,
+                "stopLiving":undefined,
+                "width":2,
+                "height":3
+            },
+            {
+                "kind":"Permanent",
+                "x":57,
+                "y":6,
+                "startLiving":0,
+                "stopLiving":undefined,
+                "width":3,
+                "height":1
+            },
+            {
+                "kind":"Permanent",
+                "x":64,
+                "y":0,
+                "startLiving":0,
+                "stopLiving":undefined,
+                "width":3,
+                "height":8
+            },
+            {
+                "kind":"Permanent",
+                "x":67,
+                "y":0,
+                "startLiving":0,
+                "stopLiving":undefined,
+                "width":2,
+                "height":6
+            },
+            {
+                "kind":"Permanent",
+                "x":69,
+                "y":0,
+                "startLiving":0,
+                "stopLiving":undefined,
+                "width":2,
+                "height":4
+            },
+            {
+                "kind":"Permanent",
+                "x":71,
+                "y":0,
+                "startLiving":0,
+                "stopLiving":undefined,
+                "width":10,
+                "height":2
             },
             {
                 "kind":"Mask",
                 "type":"wrestler",
                 "blur": "rgba(255, 255, 0, 0.9)",
-                "x":36,
-                "y":7,
+                "x":75,
+                "y":2,
                 "startLiving":0
             },
+            // {
+            //     "kind":"Mask",
+            //     "type":"ninja",
+            //     "blur": "rgba(0, 0, 0, 0.9)",
+            //     "x":75,
+            //     "y":2,
+            //     "startLiving":0
+            // },
         ],
         "endings":[
             {
                 "area":{
-                    "x": 77,
+                    "x": 100,
                     "y": 3,
                     "width":2,
                     "height":1
